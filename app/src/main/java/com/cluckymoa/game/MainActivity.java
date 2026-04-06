2c17efcd0f2885960a35e0c88ea3f2bffda711cb
brandonmathewp <brandon.mathew.p@gmail.com>
2026-04-06 13:16:07 -1000

    Initial Commit: Starting Point for Game

---
+++ app/src/main/java/com/cluckymoa/game/MainActivity.java
@@ -0,0 +1,17 @@
+package com.cluckymoa.game;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
